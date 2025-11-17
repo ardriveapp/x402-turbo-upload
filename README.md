81a982aa5cb257553324637be4eeeb4d2325c45b
Derek Sonnenberg <fedellen@pixelpajamastudios.com>
2025-11-17 14:37:51 -0600

    feat: init x402 turbo upload tool PE-8587

---
+++ README.md
@@ -0,0 +1,34 @@
+# x402-turbo-upload
+
+A simple TypeScript command line tool to upload data items to the x402 Turbo Upload Service.
+
+## Usage
+
+```sh
+yarn && yarn build
+node dist/index.js --url <UPLOAD_URL> --path <DATA_PATH> --wallet <EVM_PKEY_OR_PATH> --max-usdc <MAX_USDC_VALUE>
+```
+
+-   `DATA_PATH`: The path to the data file to be uploaded.
+-   `EVM_PKEY_OR_PATH`: The EVM private key or the path to a file containing the EVM private key used for signing.
+-   `UPLOAD_URL`: (Optional) Custom upload service URL. Defaults to `https://upload.ardrive.dev/x402/data-item/signed`.
+-   `MAX_USDC_VALUE`: (Optional) The maximum USDC value for the upload.
+
+## Example
+
+```sh
+node dist/index.js --path ./data/file.txt --wallet "0xYOUR_PRIVATE_KEY"
+```
+
+## Env vars
+
+You can alternatively set the following environment variables instead of using command line arguments:
+
+-   `UPLOAD_URL`: Custom upload service URL.
+-   `DATA_PATH`: The path to the data file to be uploaded.
+-   `EVM_PRIVATE_KEY`: The EVM private key used for signing.
+-   `MAX_USDC_VALUE`: The maximum USDC value for the upload.
+
+## Notes
+
+This is a minimal example for demonstration purposes of x402 with Turbo Upload Service API. In a production environment, ensure to handle errors, edge cases, and security considerations appropriately.
---
+++ src/index.ts
@@ -0,0 +1,104 @@
+// send x402 upload request
+
+import { readFileSync } from "node:fs";
+import { createWalletClient, http } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { Signer, wrapFetchWithPayment } from "x402-fetch";
+import { baseSepolia } from "viem/chains";
+import {
+    createData,
+    EthereumSigner,
+} from "@dha-team/arbundles/build/node/esm/index";
+
+type UploadDataParams = {
+    uploadUrl: string;
+    dataPath: string;
+    evmPrivateKey: `0x${string}`;
+    maxMUSDCValue: bigint;
+    // TODO: Consider support for tags, contentType, etc.
+};
+
+/** Load upload parameters from command line arguments, environment variables, or use testing defaults */
+function loadArguments(): UploadDataParams {
+    const defaultUploadUrl = "https://upload.ardrive.dev";
+    const defaultDataPath = "fixtures/4byte.txt";
+    const defaultEvmPkeyPath =
+        "fixtures/0x20c1DF6f3310600c8396111EB5182af9213828Dc.eth.pk.txt";
+
+    const uploadUrl = process.argv.includes("--url")
+        ? process.argv[process.argv.indexOf("--url") + 1]
+        : process.env.UPLOAD_URL || defaultUploadUrl;
+
+    const dataPath = process.argv.includes("--path")
+        ? process.argv[process.argv.indexOf("--path") + 1]
+        : process.env.DATA_PATH || defaultDataPath;
+
+    const evmWalletPathOrPkey = process.argv.includes("--wallet")
+        ? process.argv[process.argv.indexOf("--wallet") + 1]
+        : process.env.EVM_PRIVATE_KEY || defaultEvmPkeyPath;
+
+    const maxUSDCValue = BigInt(
+        process.argv.includes("--max-usdc")
+            ? process.argv[process.argv.indexOf("--max-usdc") + 1]
+            : process.env.MAX_USDC_VALUE || 1 // Default 1 USDC
+    );
+
+    let evmPrivateKey: string;
+    if (evmWalletPathOrPkey.startsWith("0x")) {
+        evmPrivateKey = evmWalletPathOrPkey;
+    } else {
+        evmPrivateKey = JSON.parse(
+            readFileSync(evmWalletPathOrPkey, "utf-8")
+        ).trim();
+    }
+
+    // TypeGuard for EVM private key format (viem/account expects `0x` prefixed string)
+    function isEVMPrivateKey(key: string): key is `0x${string}` {
+        return key.startsWith("0x") && key.length === 66; // Could import ethers for most robust check
+    }
+
+    if (!isEVMPrivateKey(evmPrivateKey)) {
+        throw new Error("Invalid EVM private key format.");
+    }
+
+    return {
+        uploadUrl,
+        dataPath,
+        evmPrivateKey,
+        maxMUSDCValue: maxUSDCValue * BigInt(1_000_000), // Convert to base units (6 decimals)
+    };
+}
+
+async function uploadData() {
+    const { evmPrivateKey, dataPath, uploadUrl, maxMUSDCValue } =
+        loadArguments();
+
+    const client = createWalletClient({
+        account: privateKeyToAccount(evmPrivateKey),
+        transport: http(),
+        chain: baseSepolia,
+    }) as unknown as Signer;
+
+    // Create ANS-104 data item from file data
+    const signer = new EthereumSigner(evmPrivateKey);
+    const dataItem = createData(readFileSync(dataPath), signer);
+    await dataItem.sign(signer);
+
+    // Make a request that may require payment
+    const fetchWithPay = wrapFetchWithPayment(fetch, client, maxMUSDCValue);
+    const response = await fetchWithPay(uploadUrl + "/x402/data-item/signed", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/octet-stream",
+        },
+        body: dataItem.getRaw() as BodyInit,
+    });
+
+    // Log response and parse the returned data
+    console.log(response);
+    console.log(await response.json());
+}
+
+uploadData().catch((error) => {
+    console.error("Error uploading data:", error);
+});
